d11be1c31f050618f7f6cc071b9f9afe9e6f0a0b
Dzhoni <patseshanka.evgeniy>
2021-12-17 13:36:59 +0300

    Add AuthService

---
+++ Mobile/GuessWord.Mobile/Services/AuthService.cs
@@ -0,0 +1,19 @@
+﻿using GuessWord.Mobile.Models;
+
+namespace GuessWord.Mobile.Services
+{
+    public class AuthService : IAuthService
+    {
+        public SignInResult TrySignIn(string login, string password)
+        {
+            var result = new SignInResult { Succeeded = true };
+            return result;
+
+        }
+
+        public SignUpResult TrySignUp(string login, string password, string username)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}

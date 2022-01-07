1953736d9e13178a8e2f595877627512caca13c1
Dzhoni <patseshanka.evgeniy>
2022-01-07 20:19:39 +0300

    Add Level controller

---
+++ Mobile/GuessWord.Mobile/Services/ILevelsController.cs
@@ -0,0 +1,10 @@
+﻿using GuessWord.Mobile.Models;
+using System.Threading.Tasks;
+
+namespace GuessWord.Mobile.Services
+{
+    public interface ILevelsController
+    {
+        Task<Level> GetLevelAsync();
+    }
+}
---
+++ Mobile/GuessWord.Mobile/Services/LevelsController.cs
@@ -0,0 +1,51 @@
+﻿using GuessWord.Mobile.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace GuessWord.Mobile.Services
+{
+    public class LevelsController : ILevelsController
+    {
+        public async Task<Level> GetLevelAsync()
+        {
+            var level = new Level();
+           
+            level.Steps = new List<Step>
+            {
+                new Step
+                {
+                    Target = "work",
+                    Options = new List<Option>
+                    {
+                        new Option{ IsCorrect = true, OrderNumber = 0, Word = "Работа" },
+                        new Option{ IsCorrect = false, OrderNumber = 1, Word = "Дом" },
+                        new Option{ IsCorrect = false, OrderNumber = 2, Word = "Семья" },
+                    } 
+                },
+                 new Step
+                {
+                    Target = "home",
+                    Options = new List<Option>
+                    {
+                        new Option{ IsCorrect = false, OrderNumber = 0, Word = "Работа" },
+                        new Option{ IsCorrect = true, OrderNumber = 1, Word = "Дом" },
+                        new Option{ IsCorrect = false, OrderNumber = 2, Word = "Семья" },
+                    }
+                },
+                  new Step
+                {
+                    Target = "family",
+                    Options = new List<Option>
+                    {
+                        new Option{ IsCorrect = false, OrderNumber = 0, Word = "Работа" },
+                        new Option{ IsCorrect = false, OrderNumber = 1, Word = "Дом" },
+                        new Option{ IsCorrect = true, OrderNumber = 2, Word = "Семья" },
+                    }
+                },
+            };
+            level.Count = level.Steps.Count;
+            await Task.Delay(3000);
+            return await Task.FromResult(level);
+        }
+    }
+}

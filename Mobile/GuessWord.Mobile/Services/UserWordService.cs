bdcf92a24a89b0c34bde73a214c70c670b9c95f5
Dzhoni <patseshanka.evgeniy>
2022-03-30 02:11:44 +0300

    mobile: change BackendClient on GuessWordApiClient for GetUserWords

--- Mobile/GuessWord.Mobile/Services/UserWordService.cs
+++ Mobile/GuessWord.Mobile/Services/UserWordService.cs
@@ -1,4 +1,5 @@
-﻿using GuessWord.Mobile.Models;
+﻿using GuessWord.Mobile.Clients;
+using GuessWord.Mobile.Models;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,16 +8,16 @@ namespace GuessWord.Mobile.Services
 {
     public class UserWordService : IUserWordService
     {
-        private readonly IBackendClient _backendClient;
+        private readonly IGuessWordApiClient _apiClient;
 
-        public UserWordService(IBackendClient backendClient)
+        public UserWordService(IGuessWordApiClient apiClient)
         {
-            _backendClient = backendClient;
+            _apiClient = apiClient;
         }
 
         public async Task<List<UserWord>> GetAllAsync()
         {
-            var userWords = await _backendClient.GetAllAsync<UserWordDto>("user-words");
+            var userWords = await _apiClient.GetUserWordsAsync();
 
 
             return userWords
@@ -32,36 +33,36 @@ namespace GuessWord.Mobile.Services
 
         public async Task<UserWord> GetAsync(int id)
         {
-            var userWordDto = await _backendClient.GetAsync<UserWordDto>($"user-words/{id}");
+            //var userWordDto = await _backendClient.GetAsync<UserWordDto>($"user-words/{id}");
 
-            var userWord = new UserWord
-            {
-                Id = userWordDto.Id,
-                Word = userWordDto.Word,
-                Status = userWordDto.Status.ToString(),
-                Translations = userWordDto.Translations.Aggregate((left, right) => left + " , " + right)
-            };
-
+            //var userWord = new UserWord
+            //{
+            //    Id = userWordDto.Id,
+            //    Word = userWordDto.Word,
+            //    Status = userWordDto.Status.ToString(),
+            //    Translations = userWordDto.Translations.Aggregate((left, right) => left + " , " + right)
+            //};
+            var userWord = new UserWord();
             return userWord;
         }
 
         public async Task<UserWord> CreateAsync(UserWord userWord)
         {
-            var userWordDto = new UserWordDto
-            {
-                Word = userWord.Word,
-                Status = Models.Enums.WordStatus.New,
-                Translations = userWord.Translations.Split(" , ").ToList()
-            };
-            userWordDto = await _backendClient.PostAsync<UserWordDto,UserWordDto>("user-words", userWordDto);
+            //var userWordDto = new UserWordDto
+            //{
+            //    Word = userWord.Word,
+            //    Status = Models.Enums.WordStatus.New,
+            //    Translations = userWord.Translations.Split(" , ").ToList()
+            //};
+            //userWordDto = await _backendClient.PostAsync<UserWordDto,UserWordDto>("user-words", userWordDto);
 
-            var newUserWord = new UserWord
-            {
-                Id = userWordDto.Id,
-                Word = userWordDto.Word,
-                Status = userWordDto.Status.ToString(),
-                Translations = userWordDto.Translations.Aggregate((left, right) => left + " , " + right)
-            };
+            //var newUserWord = new UserWord
+            //{
+            //    Id = userWordDto.Id,
+            //    Word = userWordDto.Word,
+            //    Status = userWordDto.Status.ToString(),
+            //    Translations = userWordDto.Translations.Aggregate((left, right) => left + " , " + right)
+            //};
 
             return userWord;
 

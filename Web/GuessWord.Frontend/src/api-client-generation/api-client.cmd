fa9761dd0293c1189b95aa1e89b447f71f033aab
patseshankaevgeniy <patseshanka.eugeniy@gmail.com>
2022-08-22 19:52:36 +0300

    Revert "frontend: Adding a apiClient and injecting into services"
    This reverts commit 46a27bf915f2978590953adc77ae85962a6134ed.

--- Web/GuessWord.Frontend/src/api-client-generation/api-client.cmd
+++
@@ -1,1 +0,0 @@
-"c:\Program Files (x86)\Rico Suter\NSwagStudio\Net60\dotnet-nswag.exe" run api-client.nswag
--- Web/GuessWord.Frontend/src/app/services/words.service.ts
+++ Web/GuessWord.Frontend/src/app/services/words.service.ts
@@ -1,24 +1,25 @@
 import { HttpClient, HttpHeaders, HttpParams } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { map, Observable } from "rxjs";
-import { ApiClient, IWordDto, WordDto } from "../clients/api.client";
+import { GuessWordApiClient, IWordDto, WordDto } from "../clients/GuessWordApiClient";
 import { Word } from "../models/word";
-
 
 @Injectable()
 export class WordService {
-    private apiClient: ApiClient;
+    private httpClient: HttpClient;
+    private url: string = "https://localhost:59378/api/words";
     private headers = new HttpHeaders({
         'Content-Type': 'application/json',
         'Authorization': `Bearer eyJhbGciOiJodHRwOi8vd3d3LnczLm9yZy8yMDAxLzA0L3htbGRzaWctbW9yZSNobWFjLXNoYTI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1lIjoiSWNlIiwiaHR0cDovL3NjaGVtYXMueG1sc29hcC5vcmcvd3MvMjAwNS8wNS9pZGVudGl0eS9jbGFpbXMvbmFtZWlkZW50aWZpZXIiOiIyIiwiaHR0cDovL3NjaGVtYXMubWljcm9zb2Z0LmNvbS93cy8yMDA4LzA2L2lkZW50aXR5L2NsYWltcy9yb2xlIjoidXNlciIsImV4cCI6MTY1OTM3NjU2OSwiaXNzIjoiVGVzdC5jb20iLCJhdWQiOiJUZXN0LmNvbSJ9.sagYj6SFmAYECMPPO6wr9T7k1CY3Fhm7byTY5jWXvg4`
       })
-    constructor(apiClient: ApiClient){
-      this.apiClient = apiClient;
+    constructor( httpClient: HttpClient){
+       this.httpClient = httpClient;
+      
     }
 
     public getWords(): Observable<Word[]> {
-        return this.apiClient
-            .getWords()
+        return this.httpClient
+            .get<IWordDto[]>("https://localhost:59378/api/words", {headers: this.headers})
             .pipe(map(dtos => dtos.map(dto => new Word(dto.id, dto.value!, dto.translations!))));
     }
 
@@ -27,16 +28,20 @@ export class WordService {
     }
 
     public getByLetter(letter: string): Observable<Word[]> {    
-            return  this.apiClient
-            .getWords(letter, undefined)
+            return  this.httpClient
+            .get<IWordDto[]>(this.url + "/search?" + letter, {headers: this.headers, params: new HttpParams().set('letter', letter)})
             .pipe(map(dtos => dtos.map(dto => new Word(dto.id, dto.value!, dto.translations!))));       
     }
 
-    public createWord(newWord: Word): Observable<any> {
-        var newWordDto = new WordDto({
-            value: newWord.value,
-            translations: newWord.translations
-        })
-        return this.apiClient.createWord(newWordDto);
+    public createWord(): boolean {
+        return true;
+    }
+
+    public deleteWord(): boolean {
+        return true;
+    }
+
+    public updateWord(): void {
+        
     }
 }
